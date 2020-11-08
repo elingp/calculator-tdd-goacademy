7d23cfdcdf1a6cfe9e23ad003cd022a0c732f3cd
Eling Pramuatmaja <elingpramuatmaja@gmail.com>
2020-11-08 07:46:17 +0700

    Add Calculator cancel implementation
    Cancel method will reset/cancel the calculationResult of Calculator
    object back to 0.0 and return the calculationResult. This also add a
    final variable DELTA in test that is used to assert double data type.

--- app/src/test/java/com/elingp/calculator_tdd_goacademy/CalculatorTest.java
+++ app/src/test/java/com/elingp/calculator_tdd_goacademy/CalculatorTest.java
@@ -4,10 +4,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
+  private static final double DELTA = 1e-15;
+
   @Test
   public void givenCalculatorShouldReturnZeroAsInitialValue() {
     Calculator classUnderTest = new Calculator();
     String result = classUnderTest.toString();
     assertEquals("0.0", result);
+  }
+
+  public void givenCalculatorWhenCancelShouldReturnZero() {
+    Calculator classUnderTest = new Calculator();
+    double result = classUnderTest.cancel();
+    assertEquals(0.0, result, DELTA);
   }
 }
